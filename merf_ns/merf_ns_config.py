c38154b7711d6a9d8c96aa11cd8da82a65a8b308
Sophus David <95245426@qq.com>
2023-07-02 16:21:44 +0800

    Init Project :tada:

---
+++ merf_ns/merf_ns_config.py
@@ -0,0 +1,52 @@
+from typing import Dict
+
+import tyro
+# from nerfacc import ContractionType
+
+
+from nerfstudio.cameras.camera_optimizers import CameraOptimizerConfig
+from nerfstudio.configs.base_config import ViewerConfig
+from nerfstudio.data.datamanagers.base_datamanager import VanillaDataManagerConfig
+from nerfstudio.data.dataparsers.nerfstudio_dataparser import NerfstudioDataParserConfig
+from nerfstudio.engine.optimizers import AdamOptimizerConfig, RAdamOptimizerConfig
+from nerfstudio.engine.trainer import TrainerConfig
+
+from nerfstudio.plugins.types import MethodSpecification
+from merf_ns.merf_ns import MeRFNSModel,MeRFNSConfig
+
+from merf_ns.merf_ns_pipeline import  MeRFNSPipelineConfig,MeRFNSPipeline
+
+MeRFNS_method = MethodSpecification(config=TrainerConfig(
+    method_name="MeRFNS",
+    steps_per_eval_batch=500,
+    steps_per_save=2000,
+    mixed_precision=True,
+    pipeline=MeRFNSPipelineConfig(
+        datamanager=VanillaDataManagerConfig(
+            dataparser=NerfstudioDataParserConfig(),
+            train_num_rays_per_batch=4096,
+            eval_num_rays_per_batch=4096,
+            camera_optimizer=CameraOptimizerConfig(
+                mode="SO3xR3", optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2)
+            ),
+        ),
+        model=MeRFNSConfig(eval_num_rays_per_chunk=1 << 15),
+    ),
+    optimizers={
+        "proposal_networks": {
+            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+            "scheduler": None,
+        },
+        "fields": {
+            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+            "scheduler": None,
+        },
+    },
+    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+    vis="viewer",
+),
+  description="Custom description"
+)
+
+
+
